bccb59bc263639fe847b5adadc4d2b922a5bbd72
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-06-19 10:56:02 -0700

    still working

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import { axiosWithAuth } from "../utils/axiosWithAuth" /*1. import axiosWithAuth*/ */
+
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-
-const BubblePage = () => {
+//build components below
+const BubblePage = (/*2. add props*/props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  /*3. Add your useEffect*/
+  useEffect(() => {
+    axiosWithAuth()
+    .get("/api/colors")
+    .then(res => {
+      setColorList(res.data)
+      //console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err, "sorry, but an error has occurred attempting to retrieve this pae.")
+    })
+  }, [/*need to do more research*/props])
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList /*import components*/colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
   );
 };
 
 export default BubblePage;
+
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
@@ -11,19 +11,22 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
+  const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
+    /**4. add axiosWithAuth with put request*/
+    axiosWithAuth()
+    .put('./api/colors/${colorToEdit.id}', colorToEdit)
     // where is is saved right now?
   };
 
-  const deleteColor = color => {
+  const deleteColor = (color) => {
     // make a delete request to delete this color
   };
 
@@ -31,15 +34,17 @@ const ColorList = ({ colors, updateColors }) => {
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -56,7 +61,7 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
@@ -65,10 +70,10 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             hex code:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
